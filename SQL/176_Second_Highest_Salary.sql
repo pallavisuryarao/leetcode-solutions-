dad241b70c784dd9f703eb3c8b8a2affd903862e
pallavisuryarao <pallavisuryarao@gmail.com>
2026-01-25 18:06:37 +0530

    Update 176_Second_Highest_Salary.sql

--- SQL/176_Second_Highest_Salary.sql
+++ SQL/176_Second_Highest_Salary.sql
@@ -1,1 +1,8 @@
+-- LeetCode 176: Second Highest Salary
+-- Difficulty: Easy
+-- Concept Used: Subquery, MAX()
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
 
